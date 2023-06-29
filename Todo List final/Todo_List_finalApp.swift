7107dfe60eb19e07ac4c7f98c7e39a8802d0bc51
Jude Shorbaji <jshorbaji@hillers.org>
2023-06-28 20:54:49 -0400

    Initial Commit

---
+++ Todo List final/Todo_List_finalApp.swift
@@ -0,0 +1,17 @@
+//
+//  Todo_List_finalApp.swift
+//  Todo List final
+//
+//  Created by Jude Shorbaji on 6/28/23.
+//
+
+import SwiftUI
+
+@main
+struct Todo_List_finalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
